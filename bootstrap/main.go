31340b406a72ccf06f59ccf3dd0ad54a71c1d466
Beast <anmoldh121@gmail.com>
2021-02-16 14:31:38 +0530

    discovery bug solved

--- bootstrap/main.go
+++ bootstrap/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"context"
-
-	ntraversal "github.com/libp2p-tutorial"
+	
 	"github.com/libp2p/go-libp2p"
 	host "github.com/libp2p/go-libp2p-core/host"
 	net "github.com/libp2p/go-libp2p-core/network"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	ma "github.com/multiformats/go-multiaddr"
 	log "github.com/sirupsen/logrus"
+	ma "github.com/multiformats/go-multiaddr"
+	ntraversal "github.com/libp2p-tutorial"
 )
 
 type netNotifiee struct{}
@@ -18,9 +18,9 @@ func (nn *netNotifiee) Connected(n net.Network, c net.Conn) {
 	log.Info("Connected to: %s/p2p/%s\n", c.RemoteMultiaddr(), c.RemotePeer().Pretty())
 }
 
-func (nn *netNotifiee) Disconnected(n net.Network, v net.Conn)    {}
-func (nn *netNotifiee) OpenedStream(n net.Network, v net.Stream)  {}
-func (nn *netNotifiee) ClosedStream(n net.Network, v net.Stream)  {}
+func (nn *netNotifiee) Disconnected(n net.Network, v net.Conn)   {}
+func (nn *netNotifiee) OpenedStream(n net.Network, v net.Stream) {}
+func (nn *netNotifiee) ClosedStream(n net.Network, v net.Stream) {}
 func (nn *netNotifiee) Listen(n net.Network, a ma.Multiaddr)      {}
 func (nn *netNotifiee) ListenClose(n net.Network, a ma.Multiaddr) {}
 
@@ -35,8 +35,8 @@ func createHost(ctx context.Context) (host.Host, *dht.IpfsDHT, error) {
 	if err != nil {
 		return nil, nil, err
 	}
-	// var mode dht.ModeOpt = 2
-	d, err = dht.New(ctx, h)
+	var mode dht.ModeOpt = 2
+	d, err = dht.New(ctx, h, dht.Mode(mode))
 	if err != nil {
 		return nil, nil, err
 	}
@@ -50,7 +50,7 @@ func main() {
 	if err != nil {
 		log.Error("Error creating host")
 	}
-
+	
 	basicHost.Network().Notify(&netNotifiee{})
 	node := ntraversal.CreateNode(&basicHost, kahdemlia)
 	log.Info("Host created")
